610fcd38ee85f15564f7c4a83189824480839ff2
choss <clayton.hoss@googlemail.com>
2026-03-07 19:31:52 +0100

    updating boneshock

--- BoneshocksTrackables/UI/Trackables.lua
+++ BoneshocksTrackables/UI/Trackables.lua
@@ -232,7 +232,7 @@ function BT:InitTrackables()
 					end
 				end
 				if #parts > 0 then
-					local sep = cfg.colorAmount and "|cffffffff||r" or "|"
+					local sep = " | "
 					local displayStr = table.concat(parts, sep)
 					if cfg.showName then
 						displayStr = displayStr .. " " .. item.trackables[1].slashGroup
